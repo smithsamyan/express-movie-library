d7bf5bcea13919d75e22b82ab9364fa485fc8af0
Samy Khatib <khatib.samy@gmail.com>
2022-09-27 21:33:26 +0200

    remove some simplifications

--- src/routes/movies.js
+++ src/routes/movies.js
@@ -1,40 +1,27 @@
 import express from "express";
-import movies from "../models/movie";
 const router = express.Router();
-
-const getMoviesBySearchString = (searchString) => {
-  const regex = new RegExp(searchString);
-  return movies.filter((m) => regex.test(m.title));
-};
-
-const getMoviesById = () => {
-  return movies.filter((m) => {
-    console.log(m.id);
-    return false;
-  });
-};
 
 router.get("/movies", (req, res) => {
   try {
-    res.status(200).send(movies[0]);
+    res.status(200);
   } catch (error) {
-    res.status(500).send({ message: "Interval server error" });
+    res.status(500).send({ message: "Internal server error" });
   }
 });
 
 router.get("/movies/:id", (req, res) => {
   try {
-    req.params.id === '0' ? console.log("No idea") : console.log('Get me out of here');
+    res.status(200);
   } catch (error) {
-    res.status(500).send({ message: "Interval server error" });
+    res.status(500).send({ message: "Internal server error" });
   }
 });
 
 router.get("/search", (req, res) => {
   try {
-    console.log("Need a break");
+    res.status(200);
   } catch (error) {
-    res.status(500).send({ message: "Interval server error" });
+    res.status(500).send({ message: "Internal server error" });
   }
 });
 
